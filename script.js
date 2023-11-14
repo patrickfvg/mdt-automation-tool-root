60af8c97e41f89dc6ad441692a143b85cf3125cd
patrickfvg <140896836+patrickfvg@users.noreply.github.com>
2023-11-14 16:52:14 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,35 @@
-document.getElementById('generate-button').addEventListener('click', function() {
-  var dataCenterUrl = document.getElementById('data-center-url').value;
-  var tabName = document.querySelector('input[name="spreadsheet-tab"]:checked').value;
-  var foldersToExclude = document.getElementById('folders-to-exclude').value.split('\n').join('|');
+document.addEventListener('DOMContentLoaded', function() {
+  var generateButton = document.getElementById('generate-button');
+  var copyButton = document.getElementById('copy-button');
 
-  var formula = `=SORT(FILTER(IMPORTRANGE("${dataCenterUrl}", "${tabName}!A2:A"), NOT(REGEXMATCH(IMPORTRANGE("${dataCenterUrl}", "${tabName}!A2:A"), "${foldersToExclude}"))), 1, TRUE)`;
+  if (generateButton && copyButton) {
+    generateButton.addEventListener('click', function() {
+      var dataCenterUrl = document.getElementById('data-center-url').value;
+      var tabInput = document.querySelector('input[name="spreadsheet-tab"]:checked');
+      var foldersToExcludeInput = document.getElementById('folders-to-exclude');
 
-  document.getElementById('output-area').textContent = formula;
-  document.getElementById('copy-button').style.display = 'block';
-});
+      if (dataCenterUrl && tabInput && foldersToExcludeInput) {
+        var tabName = tabInput.value;
+        var foldersToExclude = foldersToExcludeInput.value.split('\n').join('|');
+        
+        var formula = `=SORT(FILTER(IMPORTRANGE("${dataCenterUrl}", "${tabName}!A2:A"), NOT(REGEXMATCH(IMPORTRANGE("${dataCenterUrl}", "${tabName}!A2:A"), "${foldersToExclude}"))), 1, TRUE)`;
+        
+        document.getElementById('output-area').textContent = formula;
+        copyButton.style.display = 'block';
+      } else {
+        alert('Please fill in all required fields.');
+      }
+    });
 
-document.getElementById('copy-button').addEventListener('click', function() {
-  var formula = document.getElementById('output-area').textContent;
-  navigator.clipboard.writeText(formula).then(function() {
-    alert("Formula copied to clipboard!");
-  }, function(err) {
-    alert("Error in copying text: " + err);
-  });
+    copyButton.addEventListener('click', function() {
+      var formula = document.getElementById('output-area').textContent;
+      navigator.clipboard.writeText(formula).then(function() {
+        alert("Formula copied to clipboard!");
+      }, function(err) {
+        alert("Error in copying text: " + err);
+      });
+    });
+  } else {
+    console.error('Required elements not found on the page.');
+  }
 });
